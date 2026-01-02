6354dce68ef8fb13ad84a9aacae5228c1cd44df9
= <aj19122001@gmail.com>
2026-01-02 23:02:05 +0530

    Learned about createReadStream

---
+++ Networking/lecture-1/app.js
@@ -0,0 +1,13 @@
+// Networking capabilitiess of node js
+
+import dgram from "node:dgram" // for udp server (user datagram protocol)
+
+import net from "node:net"; // for tcp server (Transmission control protocol)
+
+import http from "node:http" // for http server
+
+import https from "node:https"; //for https server 
+
+import dns from "node:dns"; // for dns work
+
+// browser only support https protocol
--- Streams/lecture-3/app.js
+++ Streams/lecture-3/app.js
@@ -13,11 +13,29 @@ const readStream = fs.createReadStream('chars.txt', { highWaterMark : 4});
 // now we will learn how to use readable event 
 // readable event is fired when then data gets loaded in the internal buffer
 
+/*
 
 readStream.on('readable', () => {
     console.log(readStream.read());
 })
 
+*/
 
+// using setEncoding method to set character encoding in streams
 
+readStream.setEncoding('utf-8');
+
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+})
+
+// destroy method
+
+readStream.destroy() // will destroy the stream and readStream.on('data') won't work as the stream is destroyed
+
+// readStream.destroy will triger readStream.on('close', () =>{}) event and also 'end' event.
+
+// close event will trigger at last
+
+// we also have readStream.error() event it will be triggered when an error occur while reading the file or we pass an error object in destroy method -> readStream.destroy(new Error('error'));
 
